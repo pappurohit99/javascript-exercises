c6e0b8a05a3af5949ec0f82203e0c5ce79b3b62d
pappurohit99 <rohitpappu23@gmail.com>
2022-11-18 17:13:04 +0530

    solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+/**
+ * @param {string} text
+ */
+const reverseString = function(text) {
+    return text.split('').reverse().join('');
 };
 
 // Do not edit below this line
