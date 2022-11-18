4982fb5b8fed54c3d479637e24668fb11ff88996
pappurohit99 <rohitpappu23@gmail.com>
2022-11-18 17:34:28 +0530

    solved 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(n) {
+    if (typeof n !== "number") {
+        return "ERROR";
+    }
 
+    return (n % 400 ===0) || (n % 4 === 0 && n % 100 !== 0);
 };
 
 // Do not edit below this line
