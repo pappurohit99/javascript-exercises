c37c08609a23a647738818541f4f9b54aca7d358
pappurohit99 <rohitpappu23@gmail.com>
2022-11-18 17:31:45 +0530

    solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (beg, end) {
+    if (!(typeof beg === "number" && typeof end === "number")) {
+        return "ERROR";
+    }
+    if (beg < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    const start = Math.min(beg, end);
+    const finish = Math.max(beg, end);
+    let sum = start;
+    for (let idx = start + 1; idx <= finish; idx++) {
+        sum += idx;
+    }
+    return sum;
 };
 
 // Do not edit below this line
