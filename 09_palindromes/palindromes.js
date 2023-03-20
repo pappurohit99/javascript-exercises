154391307643356380099ebbc66981dcd554aae5
Pappu Rohit <rohitpappu23@gmail.com>
2023-03-20 22:55:36 +0530

    Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (/** @type {string} */ text) {
+    let processed = text.trim(); // remove whitespaces
+    processed = processed.toLowerCase(); // convert to lower case
+    processed = processed.replace(/[^a-zA-Z0-9]+/g, '');
+    const reversed = processed.split('').reverse().join('');
+    return processed === reversed;
 };
 
+palindromes('A car, a man, a maraca.');
 // Do not edit below this line
 module.exports = palindromes;
