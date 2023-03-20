1addf9482f6fab37128bc11134bf49afdb5ec6cb
Pappu Rohit <rohitpappu23@gmail.com>
2023-03-20 22:42:24 +0530

    Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+// @ts-check
+
+const add = function (/** @type {number} */ a, /** @type {number} */ b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (/** @type {number} */ a, /** @type {number} */ b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+/**
+ * Cumulative sum of array
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+const sum = function (arr) {
+  return arr.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (/** @type {number[]} */ arr) {
+  return arr.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (/** @type {number} */ base, /** @type {number} */ exp) {
+  return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number < 2) {
+    return 1;
+  }
+  return factorial(number - 1) * number;
 };
 
 // Do not edit below this line
