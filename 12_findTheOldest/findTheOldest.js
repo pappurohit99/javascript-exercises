22b07d439291dd7430a10135925180bb0221edd7
Pappu Rohit <rohitpappu23@gmail.com>
2023-03-20 23:13:11 +0530

    findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const CURRENT_YEAR = 2023;
+let maxAge = -Infinity;
+let oldest = {};
+const findTheOldest = function (people) {
+    people.forEach(person => {
+        if (!Object.prototype.hasOwnProperty.call(person, 'yearOfDeath')) {
+            person.yearOfDeath = CURRENT_YEAR;
+        }
+        if (person.yearOfDeath - person.yearOfBirth > maxAge) {
+            maxAge = person.yearOfDeath - person.yearOfBirth;
+            oldest = person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
