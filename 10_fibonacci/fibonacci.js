6a92a482afdc97689741df20819bd0c41177bb83
Pappu Rohit <rohitpappu23@gmail.com>
2023-03-20 23:05:01 +0530

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibMap = [1, 1];
 
+const fibonacci = function (number) {
+    number = Number(number);
+    if (number < 1) {
+        return 'OOPS';
+    }
+
+    // if (number === 1 || number === 2) {
+    //     return 1;
+    // }
+    // return fibonacci(number - 1) + fibonacci(number - 2);
+    if (fibMap.length < number) {
+        const fibVal = fibonacci(number - 1) + fibonacci(number - 2);
+        fibMap[number - 1] = fibVal;
+    }
+    return fibMap[number - 1];
 };
 
 // Do not edit below this line
